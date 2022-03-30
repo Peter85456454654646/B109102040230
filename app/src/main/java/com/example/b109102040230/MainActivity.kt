fb9699344501811c668717eeaf9a8e21d9d9a3b9
204 <204@gmail.com>
2022-03-30 13:58:43 +0800

    22222
    11111

---
+++ app/src/main/java/com/example/b109102040230/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.b109102040230
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var myt1:TextView = findViewById<TextView>(R.id.t1)
+        myt1.text="11111"
+    }
+}
