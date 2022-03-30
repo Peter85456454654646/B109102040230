7698c98a131a35184358c3fedc10ab97d4a5850f
204 <204@gmail.com>
2022-03-30 15:07:39 +0800

    p5
    6666

---
+++ app/src/main/java/com/example/b109102040230/MainActivity5.kt
@@ -0,0 +1,11 @@
+package com.example.b109102040230
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main5)
+    }
+}
