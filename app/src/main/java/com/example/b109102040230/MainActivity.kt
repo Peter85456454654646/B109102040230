a596c03c267c611f43e579d2456266c630259c63
204 <204@gmail.com>
2022-03-30 14:07:46 +0800

    button
    22222

--- app/src/main/java/com/example/b109102040230/MainActivity.kt
+++ app/src/main/java/com/example/b109102040230/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.b109102040230
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import org.w3c.dom.Text
 
@@ -11,5 +12,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var myt1:TextView = findViewById<TextView>(R.id.t1)
         myt1.text="11111"
+        var myb1:Button = findViewById<Button>(R.id.button)
     }
 }
